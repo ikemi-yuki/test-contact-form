082d5108914f94da7aa8c8ca55f65264bde7bf73
ikemi yuki <miilyu237@outlook.jp>
2025-12-16 17:23:30 +0900

    コントローラーファイル作成、コントローラー・ルーティング仮設定、ER図作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContactController::class, 'index']);
+
+Route::get('/confirm', [ContactController::class, 'confirm']);
+
+Route::get('/thanks', [ContactController::class, 'store']);
+
+Route::get('/admin', [AdminController::class, 'index']);
