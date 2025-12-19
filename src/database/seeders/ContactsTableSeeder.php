c0de5e8e06f7d7d90362f508025970fe2554c816
ikemi yuki <miilyu237@outlook.jp>
2025-12-19 10:10:44 +0900

    ファクトリ修正

--- src/database/seeders/ContactsTableSeeder.php
+++ src/database/seeders/ContactsTableSeeder.php
@@ -14,6 +14,6 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
-        Contact::factory()->count(3)->create();
+        Contact::factory()->count(35)->create();
     }
 }
